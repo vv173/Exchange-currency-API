d8d9f6b8e5a0e962472ad104f3631507142c4257
Viktor Vodnev <v.vodnev73@gmail.com>
2021-06-03 03:39:00 +0200

    added comments

--- python_api.py
+++ python_api.py
@@ -8,6 +8,7 @@ URL = 'http://api.exchangeratesapi.io/v1/latest'
 BASE_CURRENCY = 'EUR'
 SYMBOLS = 'USD,PLN,GBP,CNY,RUB'
 
+#parametry dla api get request 
 parameters = {
     'access_key': API_KEY,
     'base': BASE_CURRENCY, 
@@ -17,6 +18,7 @@ parameters = {
 response = requests.get(URL, params=parameters)
 json_response = response.json()
 
+#zamiana ciągu walut na listę
 currencies = SYMBOLS.split(',')
 
 data = [[],[]]
@@ -24,11 +26,13 @@ for currency in currencies:
     data[0].append(currency)
     data[1].append(json_response['rates'][currency])
 
+#konwertowanie uniksowego znacznika czasu na domyślną datę 
 timestamp = json_response['timestamp']                
 creation_time = datetime.datetime.fromtimestamp(
     int(timestamp)
     ).strftime('%d-%m-%Y_%H_%M_%S')
 
+#ustawiam nazwę pliku csv w formacie "nazwa - data - godzina"
 file_name = 'euro_exchange_rate_' + str(creation_time) + '.csv'
 
 with open(file_name, 'w', newline='') as csv_file:
