c719bbc53b9fdb57a450b20d6445ec140a70c875
Viktor Vodnev <v.vodnev73@gmail.com>
2022-01-03 19:14:28 +0100

    Code Optimization

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,11 @@
 FROM python:3.9.5-alpine3.12
 
-
 RUN apk add --update bash
 RUN pip install requests
 RUN apk add restic
 
-
 ENV RESTIC_PASSWORD="VVdtsc4v5QtUCNXA"
 ENV RESTIC_REPOSITORY=/srv/backup
-
 
 RUN ["mkdir", "/bin/exchange"]
 RUN ["mkdir", "/srv/backup"]
@@ -21,5 +18,3 @@ COPY root /var/spool/cron/crontabs/
 RUN chmod +x /bin/exchange/python_api.py
 RUN chmod +x /bin/exchange/restic.sh
 CMD crond -l 2 -f
-
-
--- python_api.py
+++ python_api.py
@@ -3,38 +3,49 @@
 # File name: python_api.py
 # Description: Exchange-currency-API
 # Author: Viktor Vodnev
-# Date: 05-06-2021
+# Date: 03-01-2021
 
 import requests
 import datetime
 import logging
 import json
 import csv
+import argparse
+
+def arguments():
+    logging.info("Start parsing arguments ")
+    try:
+        parser = argparse.ArgumentParser(description='Arguments get parsed via --commands')
+        parser.add_argument("-a", type=str, default=None, required=True, help="Enter the link to the API")
+        parser.add_argument("-k", type=str, default=None, required=True, help="Enter the key for the API")
+        args = parser.parse_args()
+    except:
+        logging.exception("Error reading arguments ")
+    finally:
+        logging.debug("Argument reading completed successfully")
+    return args
 
 def json_response(URL, parameters):
     logging.info("Starting json response")
     try:
         response = requests.get(URL, params=parameters)
-        json_response = response.json()
     except:
         logging.exception("Failed to make api request")
     finally:
         logging.debug("Successful api request ")
-    return json_response
+    return response.json()
 
 def data_cp(parameters, json_response):
     logging.info("Getting information from a jason file")
     try:
         #converting a string of currencies into a list
         currencies = parameters['symbols'].split(',')
-
         data = [[],[]]
         for currency in currencies:  
             data[0].append(currency)
             data[1].append(json_response['rates'][currency])
     except:
         logging.exception("Error getting information from a json file")
-
     return data
 
 #converting a Unix timestamp to the default date
@@ -50,7 +61,7 @@ def file_name(creation_time):
     file_name = 'euro_exchange_rate_' + str(creation_time) + '.csv'
     return file_name
 
-def convert_to_json(filename, data):
+def convert_to_csv(filename, data):
     try:
         with open(filename, 'w', newline='') as csv_file:
             csv_writer = csv.writer(csv_file)
@@ -62,25 +73,25 @@ def convert_to_json(filename, data):
         logging.debug("The file is completely filled with data")
 
 def main():
-    
-    API_KEY = 'c168447546cedb690c58f867c4dbc434'
-    URL = 'http://api.exchangeratesapi.io/v1/latest'
+    args = arguments()
+    API_KEY = args.k
+    URL = args.a
     BASE_CURRENCY = 'EUR'
-    SYMBOLS = 'USD,PLN,GBP,CNY,RUB'
+    CURRENCIES = 'USD,PLN,GBP,CNY,RUB'
 
     logging.basicConfig(level=logging.DEBUG, filename='exchange.log',
      format='%(asctime)s %(levelname)s:%(message)s')
 
-    #parameters for API get request
+    #parameters for API request
     parameters = {
         'access_key': API_KEY,
         'base': BASE_CURRENCY, 
-        'symbols': SYMBOLS
+        'symbols': CURRENCIES
     }
-    json_r = json_response(URL, parameters)
-    time = time_convert(json_r)
-    data = data_cp(parameters, json_r)
-    convert_to_json(file_name(time), data)
+
+    time = time_convert(json_response(URL, parameters))
+    data = data_cp(parameters, json_response(URL, parameters))
+    convert_to_csv(file_name(time), data)
 
 if __name__ == '__main__':
     main()
